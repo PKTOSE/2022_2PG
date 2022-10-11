df0b0738d43c835418a231b78124609cd46e5540
Jeongwon Lee <jungwon0719a@naver.com>
2022-10-11 14:22:02 +0900

    🔗week6 edit #1

--- week6/week6_2.c
+++ week6/week6_2.c
@@ -1,24 +1,33 @@
 //
 // Created by Arkitio on 2022-10-11.
 //
-# include "stdio.h"
+#include "stdio.h"
 
 int max(int, int);
+int fact(int);
 
-int main(){
+int main() {
     int x, y, _max;
     printf("Input two numbers(e.g. 1 2) >> ");
     scanf("%d %d", &x, &y);
     _max = max(x, y);
+    printf("%d", fact(5));
     return 0;
 }
 
-int max(int x, int y){
-    if (x >= y){
+int max(int x, int y) {
+    if (x >= y) {
         printf("%d is bigger than %d.", x, y);
         return x;
-    }else{
+    } else {
         printf("%d is bigger than %d.", y, x);
         return y;
     }
+}
+
+int fact(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n * fact(n - 1);
 }
