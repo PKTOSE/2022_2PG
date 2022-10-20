8ac31ea75e05aea87bd442a78817c80db37aa453
Jeongwon Lee <jungwon0719a@naver.com>
2022-10-21 01:56:09 +0900

    🔗week9 은행 저축 프로그램 - 통장잔고 마이너스가 안되도록 함

--- week9/week9_1.c
+++ week9/week9_1.c
@@ -14,7 +14,7 @@ int is_login = 0;// 로그인 혹은 로그아웃시 값 변경 0: 로그인 안됨, 1: 로그인 됨,
 
 int main() {
     while (1) {
-        if (is_login == 2){
+        if (is_login == 2) {
             printf("프로그램을 종료합니다.\n");
             return 0;
         }
@@ -24,18 +24,18 @@ int main() {
             continue;
         }
         transaction();
-        if (is_login == 1){ // 로그인 되어있으면 로그아웃
+        if (is_login == 1) {// 로그인 되어있으면 로그아웃
             char out = 'n';
             printf("로그아웃 하시겠습니까? (y/N) >");
             scanf("%c", &out);
             getchar();
-            switch(out){
+            switch (out) {
                 case 'y':
                     do_logout();
                     break;
                 default:
                     printf("로그아웃 안함\n");
-                    continue ;
+                    continue;
             }
         }
         break;
@@ -52,7 +52,7 @@ int do_login() {
         if (tries >= 5) {
             is_login = 2;
             printf("시도횟수를 초과했습니다. 잠시 후 다시 시도해주세요\n");
-            return 2; // 시도횟수 초과
+            return 2;// 시도횟수 초과
         }
         int pwd = 0, input = 1;
         printf("비밀번호를 입력해주세요>> ");
@@ -61,7 +61,7 @@ int do_login() {
         if (pwd == input) {
             is_login = 1;
             printf("로그인 성공!\n");
-            return 1; // 로그인 성공
+            return 1;// 로그인 성공
         } else {
             printf("비밀번호가 일치하지 않습니다.\n");
             tries++;
@@ -70,10 +70,10 @@ int do_login() {
     }
 }
 
-void do_logout(){
-    if (is_login == 1){
+void do_logout() {
+    if (is_login == 1) {
         is_login = 0;
-    }else{
+    } else {
         printf("로그인 되어있지 않습니다.\n");
     }
 }
@@ -88,7 +88,7 @@ int transaction() {
            "=========================\n",
            "입금", "출금", "거래 후 잔액");
     savings(10000);
-    savings(-10000);
+    savings(-20000);
     savings(-10000);
     printf("=========================\n");
     return 0;
@@ -99,7 +99,7 @@ int transaction() {
  */
 void savings(long amount) {
     static long balance = 0;
-    if (amount <= 0 && balance == 0) {
+    if ((amount <= 0 && balance == 0) || balance + amount <= 0) {
         printf("더이상 출금할 수 없습니다. 잔액을 확인해 주십시오.\n");
         return;
     } else {
